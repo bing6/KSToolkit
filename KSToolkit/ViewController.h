e01290b8c3211564cd0d9045c7dba77f4a5bfecd
Howard <bing.hao1982@gmail.com>
2015-09-09 16:01:58 +0800

    init

---
+++ KSToolkit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSToolkit
+//
+//  Created by bing.hao on 15/9/9.
+//  Copyright (c) 2015年 bing.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
