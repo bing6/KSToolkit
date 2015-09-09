c59f8ceab4888c547ce1a566adbb2b79c6d41497
Howard <bing.hao1982@gmail.com>
2015-09-09 18:02:17 +0800

    update

--- KSToolkit/ViewController.m
+++ KSToolkit/ViewController.m
@@ -18,22 +18,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    CGSize s1 = CGSizeMake(1334, 768);
-    CGSize s2 = CGSizeMake(320, 480);
-    
-    NSLog(@"%f", s1.width * s1.height);
-    NSLog(@"%f", s2.width * s2.height);
-    
-    NSLog(@"%f", (s2.width + s2.height) / (s1.width + s1.height));
-    NSLog(@"%f", (s1.width * s1.height) / (s2.width * s2.height));
-    
-    NSLog(@"%@", NSStringFromCGSize(CGSizeMake((s2.width + s2.height) / (s1.width + s1.height) * s1.width, (s2.width + s2.height) / (s1.width + s1.height) * s1.height)));
-    
-    
-    [self.view onClickEvent:^{
-        NSLog(@"1");
-    }];
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ src/KSToolkit.h
@@ -0,0 +1,26 @@
+//
+//  KSToolkit.h
+//  KSToolkit
+//
+//  Created by bing.hao on 15/9/9.
+//  Copyright (c) 2015年 bing.hao. All rights reserved.
+//
+
+//Macros
+#import "KSMacros.h"
+//NS
+#import "NSArray+KS.h"
+#import "NSDate+KS.h"
+#import "NSDictionary+KS.h"
+#import "NSMutableArray+KS.h"
+#import "NSMutableDictionary+KS.h"
+#import "NSString+KS.h"
+#import "NSURL+KS.h"
+//UI
+#import "UIImage+KS.h"
+#import "UIImageView+KS.h"
+#import "UITableView+KS.h"
+#import "UIView+KS.h"
+#import "UIButton+KS.h"
+
+
